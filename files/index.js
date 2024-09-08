d6b94ad6ffe6980e65f2f9485f7e4386ce080c6b
shufeng2012 <277665039@qq.com>
2024-09-08 14:39:54 +0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -5,7 +5,7 @@ JavaScript函数
 // 加入执勤队
 var dates = [];
 
-if (localStorage.getItem("data")) { 
+if (localStorage.getItem("data")) {
     dates = JSON.parse(localStorage.getItem("data")); // 把json数据转为js对象
 } else {
     localStorage.setItem("data", JSON.stringify(dates));
@@ -31,10 +31,10 @@ function ShiXi() {
     var shixi_duiyuan_name = document.getElementById('shixi_duiyuan_name').value;
     var shixi_daoshi_class = document.getElementById('shixi_daoshi_class').value;
     var shixi_daoshi_name = document.getElementById('shixi_daoshi_name').value;
-    var index = dates.findIndex(item => 
-        item.class === shixi_duiyuan_class && 
-        item.name === shixi_duiyuan_name && 
-        item.state === "未实习"
+    var index = dates.findIndex(item =>
+        item.class === shixi_duiyuan_class &&
+        item.name === shixi_duiyuan_name &&
+        item.state === "未实习" || "已实习" || "已转正"
     );
     if (index !== -1) {
         dates[index] = {
@@ -57,10 +57,10 @@ function ZhuanZheng() {
     var zhuanzheng_daoshi_name = document.getElementById('zhuanzheng_daoshi_name').value;
     var zhuanzheng_duiyuan_class = document.getElementById('zhuanzheng_duiyuan_class').value;
     var zhuanzheng_duiyuan_name = document.getElementById('zhuanzheng_duiyuan_name').value;
-    var index = dates.findIndex(item => 
-        item.class === zhuanzheng_duiyuan_class && 
-        item.name === zhuanzheng_duiyuan_name && 
-        item.state === "未转正"
+    var index = dates.findIndex(item =>
+        item.class === zhuanzheng_duiyuan_class &&
+        item.name === zhuanzheng_duiyuan_name &&
+        item.state === "未实习" || "已实习" || "已转正"
     );
     if (index !== -1) {
         dates[index] = {
@@ -81,8 +81,8 @@ function ZhuanZheng() {
 function TiChu() {
     var tichu_class = document.getElementById('tichu_class').value;
     var tichu_name = document.getElementById('tichu_name').value;
-    var index = dates.findIndex(item => 
-        item.class === tichu_class && 
+    var index = dates.findIndex(item =>
+        item.class === tichu_class &&
         item.name === tichu_name
     );
     if (index !== -1) {
